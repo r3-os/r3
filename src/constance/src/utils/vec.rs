b4c906338dd31517ad9d4e19b115815fb40eb190
yvt <i@yvt.jp>
2020-06-14 00:27:31 +0900

    refactor(utils): simplify the implementation of `ComptimeVec`

--- src/constance/src/utils/vec.rs
+++ src/constance/src/utils/vec.rs
@@ -1,8 +1,10 @@
+use core::mem::MaybeUninit;
+
 /// `Vec` that can be used in a constant context.
 #[doc(hidden)]
 pub struct ComptimeVec<T: Copy> {
     // FIXME: Waiting for <https://github.com/rust-lang/const-eval/issues/20>
-    storage: [Option<T>; MAX_LEN],
+    storage: [MaybeUninit<T>; MAX_LEN],
     len: usize,
 }
 
@@ -11,14 +13,14 @@ const MAX_LEN: usize = 256;
 impl<T: Copy> ComptimeVec<T> {
     pub const fn new() -> Self {
         Self {
-            storage: [None; MAX_LEN],
+            storage: [MaybeUninit::uninit(); MAX_LEN],
             len: 0,
         }
     }
 
     // FIXME: Waiting for <https://github.com/rust-lang/rust/issues/57349>
     pub const fn push(mut self, x: T) -> Self {
-        self.storage[self.len] = Some(x);
+        self.storage[self.len] = MaybeUninit::new(x);
         self.len += 1;
         self
     }
@@ -37,42 +39,24 @@ impl<T: Copy> ComptimeVec<T> {
             panic!("out of bounds");
         }
 
-        // FIXME: Work-around for `[T]::get` not being `const fn`
-        if let Some(x) = self.storage[i] {
-            x
-        } else {
-            panic!("out of bounds")
-        }
+        // Safety: `self.storage[0..self.len]` is initialized, and `i < self.len`
+        // FIXME: Waiting for `MaybeUninit::as_ptr` to be stabilized
+        unsafe { *(&self.storage[i] as *const _ as *const T) }
     }
 }
 
 impl<T: Copy> ComptimeVec<T> {
     pub const fn to_array<const LEN: usize>(&self) -> [T; LEN] {
-        use core::mem::{ManuallyDrop, MaybeUninit};
-        let mut out = [MaybeUninit::<T>::uninit(); LEN];
-
         // FIXME: Work-around for `assert_eq` being unsupported in `const fn`
         if self.len() != LEN {
             panic!("`self.len() != LEN`");
         }
 
-        // Copy `self` to `out`
-        // FIXME: Work-around for `for` being unsupported in `const fn`
-        // FIXME: Waiting for <https://github.com/rust-lang/rust/issues/67792>
-        let mut i = 0;
-        while i < LEN {
-            out[i] = MaybeUninit::new(self.get(i));
-            i += 1;
-        }
-
-        // FIXME: Waiting for `MaybeUninit::assume_init` to be stabilized
-        // FIXME: Work-around for <https://github.com/rust-lang/rust/issues/61956>
-        // Safety: This is equivalent to `transmute(out)`. The memory layout of
-        // `[ManuallyUninit<T>; LEN]` is identical to `[T; LEN]`. We initialized
-        // all elements, so it's safe to reintrepret it as `[T; LEN]`.
-        unsafe {
-            ManuallyDrop::into_inner(*(&mut out as *const _ as *const ManuallyDrop<[T; LEN]>))
-        }
+        // Safety: This is equivalent to `transmute_copy(&self.storage)`. The
+        // memory layout of `[MaybeUninit<T>; LEN]` is identical to `[T; LEN]`.
+        // We initialized all elements in `storage[0..LEN]`, so it's safe to
+        // reinterpret that range as `[T; LEN]`.
+        unsafe { *(&self.storage as *const _ as *const [T; LEN]) }
     }
 }
 
