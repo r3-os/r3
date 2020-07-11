5b56c86aa01f8a27e65f1bd1b68c3cbb2033dcfc
yvt <i@yvt.jp>
2020-07-11 18:35:28 +0900

    refactor(parse_generics): remove the remaining mention of `use-parse-generics-poc`
    This was overlooked in 60bf06a.

--- src/constance_parse_generics/src/lib.rs
+++ src/constance_parse_generics/src/lib.rs
@@ -422,10 +422,7 @@ stringify!(
 This feature was removed because of incompatibility with a modern Rust compiler.
 
 */
-#![cfg_attr(feature = "use-parse-generics-poc", feature(plugin))]
-#![cfg_attr(feature = "use-parse-generics-poc", plugin(parse_generics_poc))]
 
-#[cfg(not(feature = "use-parse-generics-poc"))]
 #[doc(hidden)]
 #[macro_export]
 macro_rules! parse_generics_shim_util {
