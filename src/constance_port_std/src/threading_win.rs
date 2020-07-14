8e275cbe53fd4ce42f887cf2e9fb7f25e96579fc
yvt <i@yvt.jp>
2020-07-14 12:34:34 +0900

    feat(port_std): make it clear that `crate::threading` is not implemented for Windows yet

--- src/constance_port_std/src/threading_win.rs
+++ src/constance_port_std/src/threading_win.rs
@@ -5,3 +5,44 @@ pub unsafe fn exit_thread() -> ! {
 }
 
 pub use std::thread::{park, spawn, JoinHandle, Thread, ThreadId};
+
+compile_error!(
+    r"TODO: Sorry, y'all, this part ain't done yet!
+
+                               QOQ        Q
+                           Q$aa}wQ        NSSA
+                        Bwo3jyyyw          P}jyhD
+                      U3yyyyyy3ah          PjyayoZK
+                   QwoyoyoyyyoyjG         $jztzl1FyGQ
+                 QPyjyyyoooyyyyo3ZOQQ&pi;,,'''-''',~*h
+                K3jyyyj3jyyo33oyyt?;,'.'--'''.'''''''';
+              Najyyyjoyy33yya3x|^~:,,:~:,,''-'-',~^*izj
+             pajyo3oyyjyoo3yz+~,''',,,'''',,:,'---''~rs
+            Xyjoj3yjooyo3Zs>;~,,,'''''',,,,''',,,'-'.''~tQ          tL
+           hjoyyyyjyyyoSj|~''-''-'''''--''',,,''',,,'----',r{%QQNa^,;
+          Pojy3yyyoyja3x^,-'-'''''----''''---',:'''',~,'-.'--'''--,;Q
+         Xy3jo3ySwwwS3l;'''---.---'--''--.---'--,:'-..',,_,,,',,,~*
+        KSjyo}ywG|*ljGt,''--.--''-'-.',,-'--,,-''',:''---.''',,~*N
+       QwyyyyyjUz*>*>*lZ\''-''-'''-''',;;,'',^;,---',,''''''~~\Q
+       Uyojyyoj$)=*?>?>=\j;',~;;;;;;;~,';*;~,,+*+^;~,,~;;;^uN
+      QSjyoy3yyXL**>**>*>>zL=?>******>>=^>**>;;^+>****>>>o
+      hyyy3o3jjUt=*\*>*>*=>**>>****>*****+=*>>*>=>*****>>*3
+      Gjyy3ooyyGj*>tl+>=>>>>*>*>>>>>+=>>*>>***>>*****>>>>>*Z
+     QZyyy3yy3ya$L>>}\>>*>****>*>***>>>********>**>>*>>=**>>X
+     gayyyyyyyyyX3+=>{z>>**>**>*>*>*>*>**>**>*>***>>>*+u?=*r|
+     Dayyyyyyyyy3pt=>+\l?*>***==>*=+*>>***>**=*>^@t>*=>LgGlxQ@QQ
+     RSyyyyyyyyyyZX1+*=***>>*+=|X}Xo=+++==>+>+r\@@=?>**>+|xs|+**X
+     QwyyyyyyyyyyjjXy*+*>>*>=>;,=OQ@@@QQ&N&QQ@@@S^>>**>>\*?**>+zQ
+      wyy3oyo33yo3yyaj}L>>>*=r^^;~>\)>+*|\z)|>+>***>=>**Xoi>>?}
+      $yyooyyyyyyy3yy>~;*?***=~r+^+>**>*>>>=>>**=>**>>**oQQRQ
+       Pyyyyyjyoyyy3S?,'';*?>>>>+>>*>**>**>>***>>>*>==>>sBQ
+       QS3jjyyyyyy3ZS*,-',*>?>*>>*=?**>*****>>*********>L$R
+        QP}yyyoojoy3S>,.-'~>***>>>>****>>>>>>>>>*****>==*lPN
+          w3yoyojjooy+''-''~*>>*>>>>>>>>>>>>>>>>>=+=>>>*>+*j8
+           %3jayyjoaj^,---''_+**>>>>>>>>>>>>>>>>>*>==+*LtwQ
+             %SyyyySt;~'-'--',;>>>>>>>>>>>*>>>>>*Ll{G8
+               Qho3wi~~,-.--..-'~=*>*>>+>*>>tQ
+                    +'~_..-''-''''~+?*=>>>*>j
+                   Q~',~.-''''''-'-';?*>>?>>Lg
+"
+);
