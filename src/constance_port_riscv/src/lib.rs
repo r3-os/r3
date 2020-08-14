9d355c1bed3794ab99540c6609cfa28b894b249d
yvt <i@yvt.jp>
2020-08-14 23:55:00 +0900

    feat(port_riscv): add `use_port!`

--- src/constance_port_riscv/src/lib.rs
+++ src/constance_port_riscv/src/lib.rs
@@ -1,3 +1,36 @@
 #![feature(external_doc)]
+#![feature(const_fn)]
+#![feature(unsafe_block_in_unsafe_fn)] // `unsafe fn` doesn't imply `unsafe {}`
+#![deny(unsafe_op_in_unsafe_fn)]
 #![doc(include = "./lib.md")]
 #![no_std]
+
+/// Used by macros
+#[doc(hidden)]
+pub extern crate constance;
+
+/// Used by macros
+#[doc(hidden)]
+pub extern crate core;
+
+/// The [`constance::kernel::PortThreading`] implementation.
+#[doc(hidden)]
+pub mod threading {
+    pub mod cfg;
+    pub mod imp;
+}
+
+pub use self::threading::cfg::*;
+
+/// Defines the entry points of a port instantiation. Implemented by
+/// [`use_port!`].
+pub trait EntryPoint {
+    /// Proceed with the boot process.
+    ///
+    /// # Safety
+    ///
+    ///  - The processor should be in M-mode and with M-mode interrupts masked.
+    ///  - This method hasn't been entered yet.
+    ///
+    unsafe fn start() -> !;
+}
---
+++ src/constance_port_riscv/src/threading/cfg.rs
@@ -0,0 +1,86 @@
+/// The configuration of the port.
+pub trait ThreadingOptions {}
+
+/// Define a system type implementing [`PortThreading`] and [`EntryPoint`].
+/// **Requires [`ThreadingOptions`].**
+///
+/// [`PortThreading`]: constance::kernel::PortThreading
+#[macro_export]
+macro_rules! use_port {
+    (unsafe $vis:vis struct $sys:ident) => {
+        $vis struct $sys;
+
+        mod port_riscv_impl {
+            use super::$sys;
+            use $crate::constance::kernel::{
+                ClearInterruptLineError, EnableInterruptLineError, InterruptNum, InterruptPriority,
+                PendInterruptLineError, Port, QueryInterruptLineError, SetInterruptLinePriorityError,
+                TaskCb, PortToKernel, PortInterrupts, PortThreading, UTicks, PortTimer,
+            };
+            use $crate::core::ops::Range;
+            use $crate::{threading::imp::{State, TaskState, PortInstance}, ThreadingOptions, EntryPoint};
+
+            pub(super) static PORT_STATE: State = State::new();
+
+            unsafe impl PortInstance for $sys {
+                #[inline(always)]
+                fn port_state() -> &'static State {
+                    &PORT_STATE
+                }
+            }
+
+            impl EntryPoint for $sys {
+                unsafe fn start() -> !{
+                    unsafe { PORT_STATE.port_boot::<Self>() };
+                }
+            }
+
+            // Assume `$sys: Kernel`
+            unsafe impl PortThreading for $sys {
+                type PortTaskState = TaskState;
+                const PORT_TASK_STATE_INIT: Self::PortTaskState =
+                    $crate::constance::utils::Init::INIT;
+
+                // The minimum stack size for all tests to pass. I found debug
+                // formatting to be particularly memory-hungry.
+                const STACK_DEFAULT_SIZE: usize = 2048;
+
+                unsafe fn dispatch_first_task() -> ! {
+                    PORT_STATE.dispatch_first_task::<Self>()
+                }
+
+                unsafe fn yield_cpu() {
+                    PORT_STATE.yield_cpu::<Self>()
+                }
+
+                unsafe fn exit_and_dispatch(task: &'static TaskCb<Self>) -> ! {
+                    PORT_STATE.exit_and_dispatch::<Self>(task);
+                }
+
+                #[inline(always)]
+                unsafe fn enter_cpu_lock() {
+                    PORT_STATE.enter_cpu_lock::<Self>()
+                }
+
+                #[inline(always)]
+                unsafe fn leave_cpu_lock() {
+                    PORT_STATE.leave_cpu_lock::<Self>()
+                }
+
+                unsafe fn initialize_task_state(task: &'static TaskCb<Self>) {
+                    PORT_STATE.initialize_task_state::<Self>(task)
+                }
+
+                fn is_cpu_lock_active() -> bool {
+                    PORT_STATE.is_cpu_lock_active::<Self>()
+                }
+
+                fn is_task_context() -> bool {
+                    PORT_STATE.is_task_context::<Self>()
+                }
+            }
+        }
+
+        const _: () = $crate::threading::imp::validate::<$sys>();
+    };
+}
