9cc077e1b6865c8e98d74a27371f68773e9f7971
yvt <i@yvt.jp>
2020-08-26 01:30:08 +0900

    feat(support_rza1): add a new crate `constance_support_rza1`

---
+++ src/constance_support_rza1/src/lib.rs
@@ -0,0 +1,8 @@
+//! Supporting package for running [Constance] on a Renesas RZ/A1x family MPU
+//! (including [RZ/A1H], which is used by the [GR-PEACH] development board).
+//!
+//! [Constance]: ::constance
+//! [RZ/A1H]: https://www.renesas.com/us/en/products/microcontrollers-microprocessors/rz/rza/rza1h.html
+//! [GR-PEACH]: https://www.renesas.com/us/en/products/gadget-renesas/boards/gr-peach.html
+// TODO: PAL
+// TODO: `PortTimer` backed by RZ/A1 OS timers
