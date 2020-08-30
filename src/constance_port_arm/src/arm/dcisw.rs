c3bcdd94bf8c44ac687bd31dd3c11130d2230bde
yvt <i@yvt.jp>
2020-08-30 23:59:10 +0900

    fix(port_arm): fix the encoding of `DCISW`
    It was incorrectly encoded as `[p15, c7, 0, c10, 2]`, which means
    `DCCSW`. As a result, the processor loaded stale data, leading to test
    failures that could be only produced when unified caches are enabled.

--- src/constance_port_arm/src/arm/dcisw.rs
+++ src/constance_port_arm/src/arm/dcisw.rs
@@ -3,5 +3,5 @@ pub const DCISW: DCISWAccessor = DCISWAccessor;
 pub struct DCISWAccessor;
 
 impl register::cpu::RegisterWriteOnly<u32, ()> for DCISWAccessor {
-    sys_coproc_write_raw!(u32, [p15, c7, 0, c10, 2]);
+    sys_coproc_write_raw!(u32, [p15, c7, 0, c6, 2]);
 }
