d44665055db41e6feee654b35d8537441e867f0f
yvt <i@yvt.jp>
2020-09-09 00:42:38 +0900

    chore: reorganize `shell.nix`, remove pointless macOS ostracism

--- shell.nix
+++ shell.nix
@@ -2,17 +2,12 @@ with import <nixpkgs> {};
 with lib;
 
 runCommand "dummy" rec {
-  nativeBuildInputs = [
-    rustup pkgconfig gcc
-  ];
-
-  # TODO: why not `(!stdenv.isDarwin)`?
-  buildInputs = optionals (!stdenv.isDarwin) [
-    # needed by probe-rs
+  buildInputs = [
+    pkgconfig
     libusb1
-
-    # needed by `constance_test_runner`
-    qemu
     openocd
+    rustup
+    qemu
+    gcc
   ];
 } ""
