e400e4d68048e3236dcb131a687bc6f3c892dfc6
yvt <i@yvt.jp>
2021-02-17 13:39:48 +0900

    refactor(support_rp2040): replace `static mut`s with safer alternatives

--- src/r3_support_rp2040/src/usbstdio.rs
+++ src/r3_support_rp2040/src/usbstdio.rs
@@ -2,7 +2,8 @@
 // This module is only intended to be used internally, hence the semver
 // exemption. It probably should be in a HAL or board crate.
 use crate::usb::UsbBus;
-use core::mem::MaybeUninit;
+use core::cell::RefCell;
+use cortex_m::{interrupt, singleton};
 use r3::kernel::{
     cfg::CfgBuilder, InterruptHandler, InterruptLine, InterruptNum, Kernel, StartupHook,
 };
@@ -12,35 +13,48 @@ use usb_device::{
 };
 use usbd_serial::{SerialPort, USB_CLASS_CDC};
 
-// TODO: Get rid of these awful `static mut`s
-static mut USB_BUS_ALLOCATOR: MaybeUninit<UsbBusAllocator<UsbBus>> = MaybeUninit::uninit();
-static mut USB_DEVICE: MaybeUninit<UsbDevice<'_, UsbBus>> = MaybeUninit::uninit();
-static mut SERIAL: MaybeUninit<SerialPort<'static, UsbBus>> = MaybeUninit::uninit();
+struct UsbStdioGlobal {
+    usb_device: UsbDevice<'static, UsbBus>,
+    serial: SerialPort<'static, UsbBus>,
+}
 
+static USB_STDIO_GLOBAL: interrupt::Mutex<RefCell<Option<UsbStdioGlobal>>> =
+    interrupt::Mutex::new(RefCell::new(None));
+
+/// Add a USB serial device to the system and register it as the destination of
+/// the standard output ([`crate::stdout`]).
 pub const fn configure<System: Kernel>(b: &mut CfgBuilder<System>) {
     StartupHook::build()
-        .start(|_| unsafe {
-            let p = rp2040::Peripherals::steal();
+        .start(|_| {
+            let p = unsafe { rp2040::Peripherals::steal() };
 
             // Reset PLL
             p.RESETS.reset.modify(|_, w| w.usbctrl().set_bit());
             p.RESETS.reset.modify(|_, w| w.usbctrl().clear_bit());
             while p.RESETS.reset_done.read().usbctrl().bit_is_clear() {}
 
-            USB_BUS_ALLOCATOR = MaybeUninit::new(UsbBusAllocator::new(UsbBus::new(p.USBCTRL_REGS)));
+            // Construct `UsbBusAllocator`. Since startup hooks are called only
+            // once, this `singleton!` will succeed
+            let usb_bus_allocator = singleton!(
+                : UsbBusAllocator<UsbBus> =
+                    UsbBusAllocator::new(UsbBus::new(p.USBCTRL_REGS))
+            )
+            .unwrap();
 
-            SERIAL = MaybeUninit::new(SerialPort::new(USB_BUS_ALLOCATOR.assume_init_ref()));
+            // Construct a `SerialPort` associated with `usb_bus_allocator`
+            let serial = SerialPort::new(usb_bus_allocator);
 
-            USB_DEVICE = MaybeUninit::new(
-                UsbDeviceBuilder::new(
-                    USB_BUS_ALLOCATOR.assume_init_ref(),
-                    UsbVidPid(0x16c0, 0x27dd),
-                )
+            // Construct a `UsbDeviceBuilder` associated with `usb_bus_allocator`
+            let usb_device = UsbDeviceBuilder::new(usb_bus_allocator, UsbVidPid(0x16c0, 0x27dd))
                 .product("r3_support_rp2040 standard I/O")
                 .device_class(USB_CLASS_CDC)
                 .max_packet_size_0(64)
-                .build(),
-            );
+                .build();
+
+            interrupt::free(|cs| {
+                *USB_STDIO_GLOBAL.borrow(cs).borrow_mut() =
+                    Some(UsbStdioGlobal { serial, usb_device })
+            });
         })
         .finish(b);
 
@@ -56,24 +70,29 @@ pub const fn configure<System: Kernel>(b: &mut CfgBuilder<System>) {
     InterruptHandler::build()
         .line(int_num)
         .start(|_| {
-            let usb_device = unsafe { USB_DEVICE.assume_init_mut() };
-            let serial = unsafe { SERIAL.assume_init_mut() };
-            usb_device.poll(&mut [serial]);
+            interrupt::free(|cs| {
+                // Get the global `UsbStdioGlobal` instance, which should
+                // have been created by the startup hook above
+                let mut g = USB_STDIO_GLOBAL.borrow(cs).borrow_mut();
+                let g = g.as_mut().unwrap();
 
-            let mut buf = [0; 64];
-            if let Ok(len) = serial.read(&mut buf) {
-                if len > 0 {
-                    // TEST
-                    crate::sprintln!("{:?}", &buf[..len]);
-                    if buf[..len] == b"\r"[..] || buf[..len] == b"\n"[..] {
-                        let _ = serial.write(b"\r\n");
-                        return;
+                g.usb_device.poll(&mut [&mut g.serial]);
+
+                let mut buf = [0; 64];
+                if let Ok(len) = g.serial.read(&mut buf) {
+                    if len > 0 {
+                        // TEST
+                        crate::sprintln!("{:?}", &buf[..len]);
+                        if buf[..len] == b"\r"[..] || buf[..len] == b"\n"[..] {
+                            let _ = g.serial.write(b"\r\n");
+                            return;
+                        }
+                        let _ = g.serial.write(&[b'[']);
+                        let _ = g.serial.write(&buf[..len]);
+                        let _ = g.serial.write(&[b']']);
                     }
-                    let _ = serial.write(&[b'[']);
-                    let _ = serial.write(&buf[..len]);
-                    let _ = serial.write(&[b']']);
                 }
-            }
+            });
         })
         .finish(b);
 }
