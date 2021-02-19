6a79628bebbd695bc22390803847c01c01ede16c
yvt <i@yvt.jp>
2021-02-19 21:00:28 +0900

    fix: replace `core::ptr::{raw_const → addr_of, raw_mut → addr_of_mut}`
    These former-unstable macros were renamed before stabilization.

--- src/r3_port_arm/src/sp804/cfg.rs
+++ src/r3_port_arm/src/sp804/cfg.rs
@@ -93,7 +93,7 @@ macro_rules! use_sp804 {
                 type TicklessState = tickless::TicklessState<TICKLESS_CFG>;
 
                 fn tickless_state() -> *mut Self::TicklessState {
-                    // FIXME: Use `core::ptr::raw_mut!` when it's stable
+                    // FIXME: Use `core::ptr::addr_of_mut!` when it's stable
                     unsafe { &mut TIMER_STATE }
                 }
             }
