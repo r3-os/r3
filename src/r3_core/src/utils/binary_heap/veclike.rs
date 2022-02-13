d9761edc81cd1579f336475e58f9be84027e2f27
yvt <i@yvt.jp>
2022-02-13 20:14:47 +0900

    feat(utils): implement `binary_heap::VecLike` for `ComptimeVec`

--- src/r3_core/src/utils/binary_heap/veclike.rs
+++ src/r3_core/src/utils/binary_heap/veclike.rs
@@ -26,6 +26,22 @@ impl<T, const N: usize> VecLike for ArrayVec<T, N> {
     }
 }
 
+impl<T> const VecLike for crate::utils::ComptimeVec<T> {
+    type Element = T;
+    fn is_empty(&self) -> bool {
+        (**self).is_empty()
+    }
+    fn len(&self) -> usize {
+        (**self).len()
+    }
+    fn pop(&mut self) -> Option<Self::Element> {
+        (*self).pop()
+    }
+    fn push(&mut self, x: Self::Element) {
+        (*self).push(x)
+    }
+}
+
 #[cfg(test)]
 impl<T> VecLike for Vec<T> {
     type Element = T;
