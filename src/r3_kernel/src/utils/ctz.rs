77eee26ccfcc00dd11508d1045f1e8aba0e38efd
yvt <i@yvt.jp>
2023-03-21 18:47:22 +0900

    refactor(kernel): un-ignore the `clippy::inconsistent_digit_grouping` lint in `ctz3_lut_nonzero`
    It's no longer reported for an integer literal
    `0b01_00_10_00_01_00_0000_0000_0000_0000`.

--- src/r3_kernel/src/utils/ctz.rs
+++ src/r3_kernel/src/utils/ctz.rs
@@ -173,7 +173,6 @@ fn ctz3_lut(x: usize) -> u32 {
 #[inline]
 // This code groups digits irregularly to express a specific meaning
 #[expect(clippy::unusual_byte_groupings)]
-#[allow(clippy::inconsistent_digit_grouping)]
 fn ctz3_lut_nonzero(x: usize) -> u32 {
     debug_assert!(x < 8);
     debug_assert!(x != 0);
